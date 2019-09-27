698daf3918306bea0789ca09a56fef54567e3a9a
いとうだいち <itoudaichi@itoudaichinoMacBook-Pro.local>
2019-09-27 14:09:08 +0900

    made making new group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,19 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+  
+  def create
+    
+  end
+
   def edit
   end
 
-  def new
+  private
+  def group_params
+    
   end
+  
 end
