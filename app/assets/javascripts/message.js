6b5cd89c2eb9856d986cbd441ce777472d2fc77a
いとうだいち <itoudaichi@itoudaichinoMacBook-Pro.local>
2019-10-02 17:04:31 +0900

    add processData: false in ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,6 +2,12 @@ $(function() {
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var form_data = new FormData(this);
-
+    $.ajax({
+      type: 'POST',
+      url: '/groups//messages',
+      data: form_data,
+      processData: false,
+      dataType: 'json'
+    })
   });
 });
