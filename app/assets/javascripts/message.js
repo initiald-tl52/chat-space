d5f857d36217ea706c0b2944de8b18cffff1686e
いとうだいち <itoudaichi@itoudaichinoMacBook-Pro.local>
2019-10-04 16:15:24 +0900

    add processData: false, contentType: false

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,22 @@
 $(function() {
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
+    debugger;
     var form_data = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
       type: 'POST',
       url: url,
       data: form_data,
-      processData: false,
       dataType: 'json',
+      processData: false,
       contentType: false
+    })
+    .done(function(){
+      //通信に成功した場合の処理
+    })
+    .fail(function(){
+      //通信に失敗した場合の処理
     })
   });
 });
