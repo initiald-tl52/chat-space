14a0009d06da8d7fd5b8b989a5501a43a64a14cd
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-07 14:24:22 +0900

    display name candidate

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,4 +1,13 @@
 $(function() {
+  var names_component = $("#user-search-result");
+  function append_names(user){
+    var html =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="1023" data-user-name="orihata">追加</a>
+              </div>`
+    names_component.append(html);
+  }
+
   $("#user-search-field").keyup(function(){
     var input = $("#user-search-field").val();
     if(input.length == 0){
@@ -12,9 +21,10 @@ $(function() {
     })
     .done(function(users){
       users.forEach(function(user){
-        
+        // テキストフィールド生成して、ユーザー名を一つずつ入れる。
+        append_names(user);
       });
-      // テキストフィールド生成して、ユーザー名を一つずつ入れる。
+      
     })
     .fail(function(){
 
