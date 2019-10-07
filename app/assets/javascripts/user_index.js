65f07cc4ce520fa599c70efc7f0104378c5d0426
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-07 14:55:58 +0900

    add click function on added elements

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -7,10 +7,14 @@ $(function() {
               </div>`
     names_component.append(html);
   }
+  names_component.click('button', function(){
+    console.log("click");
+  });
 
   $("#user-search-field").keyup(function(){
     var input = $("#user-search-field").val();
     if(input.length == 0){
+      names_component.empty();
       return false;
     } 
     $.ajax({
@@ -21,13 +25,12 @@ $(function() {
     })
     .done(function(users){
       users.forEach(function(user){
-        // テキストフィールド生成して、ユーザー名を一つずつ入れる。
         append_names(user);
       });
-      
     })
     .fail(function(){
-
+      alert('ユーザー検索に失敗しました');
     })
   });
+  
 });
