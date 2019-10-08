3bf112e54f5e4a3aaab3b0c7caaa7ffb125e3eb2
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-08 16:11:58 +0900

    complete incremental fanction

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -3,16 +3,32 @@ $(function() {
   function append_names(user){
     var html =`<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="1023" data-user-name="orihata">追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
               </div>`
     names_component.append(html);
   }
-  names_component.click('button', function(){
-    console.log("click");
+
+  function show_names(id,name){
+    var html =`<div class='chat-group-user'>
+                <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'>${name}</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+              </div>`
+    $('.chat-group-users').append(html);
+  }
+
+  $(document).on('click','.user-search-add', function(){
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    $(this).parent().remove();
+    show_names(id,name);
+  });
+  $(document).on('click','.user-search-remove', function(){
+    $(this).parent().remove();
   });
 
-  $("#user-search-field").keyup(function(){
-    var input = $("#user-search-field").val();
+  $("#user-search-field").on('keyup',function(){
+    var input = $(this).val();
     if(input.length == 0){
       names_component.empty();
       return false;
@@ -32,5 +48,4 @@ $(function() {
       alert('ユーザー検索に失敗しました');
     })
   });
-  
 });
