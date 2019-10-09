8b6ebb6055b8db09def8a58d8af1f54cf5555ceb
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-09 15:34:03 +0900

    complete scroll function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,7 +54,6 @@ $(function() {
   
   var reloadMessages = function() {
     last_message_id = $('.message__text:last').data('message-id');
-    console.log("last_message_id  "+last_message_id);
     $.ajax({
       url: 'api/messages',
       type: 'get',
@@ -62,17 +61,17 @@ $(function() {
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(index, message) {
-        insertHTML += appendProduct(message);
-      });
-      $('.messages').append(insertHTML);
-      console.log("messages offset   "+$('.message__text:last').offset().top);
-      $("html,body").animate({scrollTop:$('.message__text:last').offset().top});
-      console.log("scrolled");
+      if (messages.length){
+        var insertHTML = '';
+        $.each(messages, function(index, message) {
+          insertHTML += appendProduct(message);
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop:$('.message__text:last').offset().top});
+      }
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   setInterval(reloadMessages, 5000);
